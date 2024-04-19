af58c0a04cb0f1805512fd5ba1f23adc82421615
Jenn Edge <j-Edge@users.noreply.github.com>
2024-04-19 10:46:44 -0400

    Update watcher.sh
    initial mockup of watcher commands

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,22 @@
-#TODO
+#1. Define Variables: Set the namespace, deployment name, and maximum number of restarts allowed before scaling down the deployment.
+MAX_RESTARTS=3
+loop=true
+#2. Start a Loop: Begin an infinite loop that will continue until explicitly broken.
+while loop; do
+  #3. Check Pod Restarts: Within the loop, use the kubectl get pods command to retrieve the number of restarts of the pod associated with the specified deployment in the specified namespace.
+  echo kubectl get pods
+  restarts=kubectl get pods
+  #4. Display Restart Count: Print the current number of restarts to the console.
+  echo restarts
+  #5. Check Restart Limit: Compare the current number of restarts with the maximum allowed number of restarts.
+  if restarts > MAX_RESTARTS; then
+    echo 'scale down'
+    kubectl scale 0
+    loop=false
+  #6. Scale Down if Necessary: If the number of restarts is greater than the maximum allowed, print a message to the console, scale down the deployment to zero replicas using the kubectl scale command, and break the loop.
+  else
+    #7. Pause: If the number of restarts is not greater than the maximum allowed, pause the script for 60 seconds before the next check.
+    sleep 60
+  fi
+  #8. Repeat: After the pause, the script goes back to step 3. This process repeats indefinitely until the number of restarts exceeds the maximum allowed, at which point the deployment is scaled down and the loop is broken.
+done
